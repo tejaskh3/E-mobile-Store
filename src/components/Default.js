3db649d4ccb37e0fb4951d902b617001af2b783b
TEJAS <98630752+tejaskh3@users.noreply.github.com>
2023-03-06 20:11:43 +0530

    Add files via upload

---
+++ src/components/Default.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Default() {
+  return (
+    <div>
+      <h3>PageNotFound</h3>
+    </div>
+  )
+}
---
+++ src/components/ProductList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Product from './Product.js';
+export default function ProductList() {
+  return (
+    <div>
+      <h3>
+        Hello from Product List.
+      </h3>
+    </div>
+  )
+}
+
